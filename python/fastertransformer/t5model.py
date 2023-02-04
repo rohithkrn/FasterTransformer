143250daa24d3163db5c384c135e96500d4abc0e
Rohith Nallamaddi <nalrohit@amazon.com>
2023-02-04 02:00:48 +0000

    Add generate functions for t5 model

--- python/fastertransformer/t5model.py
+++ python/fastertransformer/t5model.py
@@ -10,23 +10,31 @@
 # the specific language governing permissions and limitations under the License.
 import math
 import os
+import numpy as np
 
-from transformers import T5ForConditionalGeneration
-
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 from .examples.t5.ft_decoding import FTT5DecodingWeight, FTT5Decoding, FTT5
 from .examples.t5.ft_encoder import FTT5EncoderWeight, FTT5Encoder
 from .ftmodel import InferenceModel
 
-
 class T5Model(InferenceModel):
+    STR_TO_NUMPY_TYPE_MAP = {"fp32": np.float32, "fp16": np.float16}
+
+    def __init__(self, model: str,
+                 tensor_parallel_degree: int,
+                 pipeline_parallel_degree: int, dtype:str, **kwargs):
+        super().__init__(model, tensor_parallel_degree, pipeline_parallel_degree, dtype, **kwargs)
+        self.tokenizer = T5Tokenizer.from_pretrained(self.model)
+        self.t5: FTT5 = None
+        self.batch_size = kwargs.get("batch_size", 1)
 
     def initialize(self):
 
-        self.model = T5ForConditionalGeneration.from_pretrained(self.model)
+        hf_model = T5ForConditionalGeneration.from_pretrained(self.model)
 
         # TODO: understand these
-        encoder_config = self.model.encoder.config
-        decoder_config = self.model.decoder.config
+        encoder_config = hf_model.encoder.config
+        decoder_config = hf_model.decoder.config
         encoder_config.update({"num_experts": 0})
         decoder_config.update({"num_experts": 0})
         encoder_config.update({"moe_layer_index": []})
@@ -44,7 +52,7 @@ class T5Model(InferenceModel):
             use_gated_activation=False,
             t5_with_moe=False,
             position_embedding_type=0,
-            weight_data_type=self.dtype,
+            weight_data_type=self.STR_TO_NUMPY_TYPE_MAP[self.dtype],
         )
 
         ft_decoding_weight = FTT5DecodingWeight(
@@ -55,18 +63,18 @@ class T5Model(InferenceModel):
             use_gated_activation=False,
             t5_with_moe=False,
             position_embedding_type=0,
-            weight_data_type=self.dtype,
+            weight_data_type=self.STR_TO_NUMPY_TYPE_MAP[self.dtype],
         )
 
-        ft_encoder_weight.load_from_model(self.model)
-        ft_decoding_weight.load_from_model(self.model)
+        ft_encoder_weight.load_from_model(hf_model)
+        ft_decoding_weight.load_from_model(hf_model)
 
         if self.dtype == 'fp16':
-            self.model = self.model.half()
+            hf_model = hf_model.half()
             ft_encoder_weight.to_half()
             ft_decoding_weight.to_half()
         elif self.dtype == 'bf16':
-            self.model = self.model  # bfloat inference not supported yet
+            hf_model = hf_model  # bfloat inference not supported yet
             ft_encoder_weight.to_bfloat16()
             ft_decoding_weight.to_bfloat16()
 
@@ -96,4 +104,48 @@ class T5Model(InferenceModel):
                                    position_embedding_type=0, moe_k=0,
                                    activation_type=activation_type, tie_word_embeddings=tie_word_embeddings, )
 
-        self.model = FTT5(ft_encoder, ft_decoding)
+        self.t5 = FTT5(ft_encoder, ft_decoding)
+
+    def generate(self, input_tokens, **kwargs):
+        default_args = dict(
+            inputs_embeds=None,
+            beam_width=1,
+            max_seq_len=200,
+            top_k=1,
+            top_p=0.0,
+            beam_search_diversity_rate=0.0,
+            temperature=1.0,
+            len_penalty=0.0,
+            repetition_penalty=1.0,
+            presence_penalty=None,
+            min_length=0,
+            random_seed=0,
+            is_return_output_log_probs=False,
+            is_return_cum_log_probs=False,
+            is_return_cross_attentions=False,
+            bad_words_list=None,
+            stop_words_list=None
+        )
+        default_args.update(kwargs)
+        default_args["beam_size"] = default_args["beam_width"]
+        default_args.pop("beam_width")
+        result = self.t5(input_tokens, **default_args)
+        return result
+
+    def pipeline_generate(self, inputs, **kwargs):
+        total_iter = math.ceil(len(inputs) / self.batch_size)
+        result = []
+        output_tokens = []
+        for it in range(total_iter):
+            input_batch = inputs[it * self.batch_size: self.batch_size * (it + 1)]
+            input_tokens = self.tokenizer(input_batch, return_tensors='pt', padding=True)
+            ft_decoding_outputs, ft_decoding_seq_lens = self.generate(input_tokens)
+            output_tokens.append((ft_decoding_outputs, ft_decoding_seq_lens))
+
+        for batch_token, batch_seq_len in output_tokens:
+            decoded_batch_token = []
+            for j in range(len(batch_token)):
+                decoded_batch_token.append(self.tokenizer.decode(batch_token[j][0][:batch_seq_len[j][0]], skip_special_tokens=True))
+            result.append(decoded_batch_token)
+
+        return result
